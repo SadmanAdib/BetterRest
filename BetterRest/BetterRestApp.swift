56e5381c130dbfbf16d8bcfbcfe78efc1069f73a
Sadman Adib <sadmanadib67@gmail.com>
2022-03-01 00:13:11 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sadman Adib on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
